aa57baf8fe7bc2eac88b27ce4188eaff7abf0164
Joseph Field <160516039+JosephField09@users.noreply.github.com>
2026-08-20 19:52:41 +0200

    Add: evaluate-reverse-polish-notation - submission-10

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-10.py
@@ -0,0 +1,19 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                x, y = stack.pop(), stack.pop()
+                stack.append(x + y)
+            elif c == "-":
+                x, y = stack.pop(), stack.pop()
+                stack.append(y - x)
+            elif c == "*":
+                x, y = stack.pop(), stack.pop()
+                stack.append(x * y)
+            elif c == "/":
+                x, y = stack.pop(), stack.pop()
+                stack.append(int(float(y) / x))
+            else:
+                stack.append(int(c))
+        return stack[0]
